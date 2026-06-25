ebb3d1ea9d4f1d50c8ff6670ada3e584db5e5fb7
Phillip Metzger <phillovesjoy@gmail.com>
2026-06-25 16:33:36 -0700

    Bump driver app iOS version to 1.0.1

--- driver-app/app.config.js
+++ driver-app/app.config.js
@@ -9,7 +9,7 @@ module.exports = {
   expo: {
     name: 'ReadyRoute',
     slug: 'driver-app',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/readyroute-app-icon.png',
     userInterfaceStyle: 'light',
